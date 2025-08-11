849b0882e1fe33d0076ce79a8ebc497b62dc89e5
ducc <spielerc@gmail.com>
2025-08-11 09:53:58 -0600

    Clean up USDA parser

--- Editor/Model/ModelLoader.cs
+++ Editor/Model/ModelLoader.cs
@@ -1,4 +1,4 @@
-﻿using Duccsoft.Formats;
+﻿using Duccsoft.Formats.Usd.Ascii;
 using Duccsoft.Mounting;
 using Sandbox.Mounting;
 
@@ -9,16 +9,16 @@ public class ModelLoader( MountAssetPath modelPath ) : ResourceLoader<HalfLife2R
 	protected override object Load()
 	{
 		var usdStage = new UsdaReader().ReadFromPath( modelPath.Absolute );
-		Log.Info( $"Loaded USDA file {modelPath.Relative} with {usdStage.Prims} prims:" );
-		foreach ( var prim in usdStage.Prims )
-		{
-			Log.Info( $"{prim.Type} \"{prim.Name}\" " + '{' );
-			foreach ( var (_, attribute) in prim.Attributes )
-			{
-				Log.Info( $"\t{attribute.Type}{(attribute.IsArray ? "[]" : string.Empty)} {attribute.Name}" );
-			}
-			Log.Info( '}' );
-		}
+		Log.Info( $"Loaded USDA file {modelPath.Relative} with {usdStage.Prims.Count} prims:" );
+		// foreach ( var prim in usdStage.Prims )
+		// {
+		// 	Log.Info( $"{prim.Type} \"{prim.Name}\" " + '{' );
+		// 	foreach ( var (_, attribute) in prim.Attributes )
+		// 	{
+		// 		Log.Info( $"\t{attribute.Type}{(attribute.IsArray ? "[]" : string.Empty)} {attribute.Name}" );
+		// 	}
+		// 	Log.Info( '}' );
+		// }
 		return usdStage.LoadModel();
 	}
 }
