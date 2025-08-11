6f98a0b011a4c458b866eae7677430ac2eafe456
ducc <spielerc@gmail.com>
2025-08-10 19:36:43 -0600

    WIP USD prim attribute parsing

--- Editor/Model/ModelLoader.cs
+++ Editor/Model/ModelLoader.cs
@@ -6,5 +6,19 @@ namespace HalfLife2Rtx;
 
 public class ModelLoader( MountAssetPath modelPath ) : ResourceLoader<HalfLife2RtxMount>
 {
-	protected override object Load() => new UsdaReader().ReadFromPath( modelPath.Absolute ).LoadModel();
+	protected override object Load()
+	{
+		var usdStage = new UsdaReader().ReadFromPath( modelPath.Absolute );
+		Log.Info( $"Loaded USDA file {modelPath.Relative} with {usdStage.Prims} prims:" );
+		foreach ( var prim in usdStage.Prims )
+		{
+			Log.Info( $"{prim.Type} \"{prim.Name}\" " + '{' );
+			foreach ( var (_, attribute) in prim.Attributes )
+			{
+				Log.Info( $"\t{attribute.Type}{(attribute.IsArray ? "[]" : string.Empty)} {attribute.Name}" );
+			}
+			Log.Info( '}' );
+		}
+		return usdStage.LoadModel();
+	}
 }
